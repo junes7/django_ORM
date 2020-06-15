d4cd8bde35de96165041183d73ed631d8a32d85c
junes7 <2junes@naver.com>
2020-06-15 16:44:44 +0900

    django CLRUD study by ORM

--- mysite/articles/models.py
+++ mysite/articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model) : 
+    # articles_article
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 수정이 되는 시간을 저장해준다.
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.id}번째글 - {self.title} : {self.content}'
+
+
+
